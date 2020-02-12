306c7d42b4fbdb31fc260f12930a4d60ba4c4de4
Dariusz Okonski <darek200180@gmail.com>
2020-02-12 17:00:39 +0100

    add: states and componentDidMount and mapArray function

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,37 +1,49 @@
 import React, { Component } from 'react';
 import Box from './Box';
+import uuid from 'uuid/v1';
 
 class Game extends Component {
-  state = {}
+  static numberOfClumns = 5;
+  static numberOfRows = 5;
+
+  state = {
+    boxStates: [
+      [true, false, true, false, false],
+      [true, true, true, false, true],
+      [false, false, true, false, false],
+      [true, true, true, false, false],
+      [true, false, true, false, false],
+    ],
+    singleBoxes: []
+  }
+
+  componentDidMount() {
+    this.mapArray();
+  }
+
+  mapArray = () => {
+    let singleBoxes = [];
+
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        singleBoxes.push({ name: `${i}-${j}`, on: this.state.boxStates[i][j] })
+      }
+    }
+
+    this.setState({
+      singleBoxes
+    })
+  }
 
   render() {
+    const displayBoxes = this.state.singleBoxes.map(box =>
+      <Box key={uuid()} id={box.name} isOn={box.on} />)
+
     return (
       <div className="game-board">
-        <Box isOn={true} />
-        <Box isOn={false} />
-        <Box isOn={true} />
-        <Box isOn={false} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
-        <Box isOn={true} />
+        {
+          displayBoxes
+        }
       </div>
     );
   }
