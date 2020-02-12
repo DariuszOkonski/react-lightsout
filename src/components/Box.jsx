0a1b9cdf09df2a430d078077692c02a668457ebe
Dariusz Okonski <darek200180@gmail.com>
2020-02-12 18:11:56 +0100

    add: onClick event

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Box = ({ isOn, id }) => {
+const Box = ({ isOn, id, handleBoxState }) => {
   const bolb = isOn ? 'isOn' : 'isOff';
 
-  // console.log(bolb)
-
   return (
-    <div id={id} className={`box ${bolb}`}></div>
+    <div
+      id={id}
+      className={`box ${bolb}`}
+      onClick={() => handleBoxState(id)}
+    ></div>
   );
 }
 
