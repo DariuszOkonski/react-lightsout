fc00a9bfbc027c8027aa8e39fa0a8568a498caad
Dariusz Okonski <darek200180@gmail.com>
2020-02-12 16:30:13 +0100

    add: headlights and Game component to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Game from './components/Game';
 
 function App() {
   return (
     <div className="app">
-      <h1>Lights out</h1>
+      <h3 className="headlights">
+        <span className="lights">Lights -</span><span className="out"> Out</span>
+      </h3>
+      <Game />
     </div>
   );
 }
